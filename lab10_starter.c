be13e56807a07aa2bdb9abf67210b55c07984fa5
Daphne-MU <32230740+daphne-mu@users.noreply.github.com>
2022-07-11 11:25:22 -0500

    Add files via upload

---
+++ lab10_starter.c
@@ -0,0 +1,28 @@
+/*
+ * CS1050
+ * Summer 2022
+ * Lab 10 starter file
+ */
+
+// includes
+#include <stdio.h>
+#include <ctype.h>
+
+// Symbolic Constants
+#define CLEARTEXT \
+"It's Christmastime in Washington\a\nThe Democrats rehearsed \b\n"\
+"Gettin' into gear for four more years\nThings not gettin' worse"\
+"\"\\\?\n\n\t\t- Steve Earle"
+
+// Prototypes
+void Encrypt(char *in, int displacement);
+
+// Main
+int main(void)
+{
+}
+
+// Function implementations
+void Encrypt(char *s, int displacement)
+{
+}
